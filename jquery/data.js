b62ba10c9fe6669a579d66194449629927045d33
Chandrashekar Jayaram <cjayaram@caiso.com>
2017-10-11 17:49:31 -0700

    first commit

---
+++ jquery/data.js
@@ -0,0 +1,223 @@
+var customers =[
+    {
+        "addressline1": "111 E. Las Olivas Blvd",
+        "addressline2": "Suite 51",
+        "city": "Fort Lauderdale",
+        "creditLimit": 100000,
+        "customerId": 1,
+        "discountCode": {
+            "discountCode": "N",
+            "rate": 0
+        },
+        "email": "jumboeagle@example.com",
+        "fax": "305-555-0189",
+        "name": "Jumbo Eagle Corp",
+        "phone": "305-555-0188",
+        "state": "FL",
+        "zip": "95117"
+    },
+    {
+        "addressline1": "9754 Main Street",
+        "addressline2": "P.O. Box 567",
+        "city": "Miami",
+        "creditLimit": 50000,
+        "customerId": 2,
+        "discountCode": {
+            "discountCode": "M",
+            "rate": 11
+        },
+        "email": "www.new.example.com",
+        "fax": "305-555-0149",
+        "name": "New Enterprises",
+        "phone": "305-555-0148",
+        "state": "FL",
+        "zip": "95035"
+    },
+    {
+        "addressline1": "8989 Red Albatross Drive",
+        "addressline2": "Suite 9897",
+        "city": "Houston",
+        "creditLimit": 25000,
+        "customerId": 25,
+        "discountCode": {
+            "discountCode": "M",
+            "rate": 11
+        },
+        "email": "www.wrencomp.example.com",
+        "fax": "214-555-0134",
+        "name": "Wren Computers",
+        "phone": "214-555-0133",
+        "state": "TX",
+        "zip": "85638"
+    },
+    {
+        "addressline1": "8585 South Upper Murray Drive",
+        "addressline2": "P.O. Box 456",
+        "city": "Alanta",
+        "creditLimit": 90000,
+        "customerId": 3,
+        "discountCode": {
+            "discountCode": "L",
+            "rate": 7
+        },
+        "email": "www.smallbill.example.com",
+        "fax": "555-555-0176",
+        "name": "Small Bill Company",
+        "phone": "555-555-0175",
+        "state": "GA",
+        "zip": "12347"
+    },
+    {
+        "addressline1": "65653 Lake Road",
+        "addressline2": "Suite 2323",
+        "city": "San Mateo",
+        "creditLimit": 65000,
+        "customerId": 36,
+        "discountCode": {
+            "discountCode": "H",
+            "rate": 16
+        },
+        "email": "www.bobhostcorp.example.com",
+        "fax": "650-555-0161",
+        "name": "Bob Hosting Corp.",
+        "phone": "650-555-0160",
+        "state": "CA",
+        "zip": "94401"
+    },
+    {
+        "addressline1": "829 E Flex Drive",
+        "addressline2": "Suite 853",
+        "city": "San Jose",
+        "creditLimit": 26500,
+        "customerId": 106,
+        "discountCode": {
+            "discountCode": "L",
+            "rate": 7
+        },
+        "email": "www.centralcomp.example.com",
+        "fax": "408-555-0150",
+        "name": "Early CentralComp",
+        "phone": "408-555-0157",
+        "state": "CA",
+        "zip": "95035"
+    },
+    {
+        "addressline1": "4381 Kelly Valley Ave",
+        "addressline2": "Suite 77",
+        "city": "Santa Clara",
+        "creditLimit": 70000,
+        "customerId": 149,
+        "discountCode": {
+            "discountCode": "L",
+            "rate": 7
+        },
+        "email": "www.johnvalley.example.com",
+        "fax": "408-555-0167",
+        "name": "John Valley Computers",
+        "phone": "408-555-0169",
+        "state": "CA",
+        "zip": "95117"
+    },
+    {
+        "addressline1": "456 444th Street",
+        "addressline2": "Suite 45",
+        "city": "Redwood City",
+        "creditLimit": 25000,
+        "customerId": 863,
+        "discountCode": {
+            "discountCode": "N",
+            "rate": 0
+        },
+        "email": "www.bignet.example.com",
+        "fax": "650-555-0180",
+        "name": "Big Network Systems",
+        "phone": "650-555-0181",
+        "state": "CA",
+        "zip": "94401"
+    },
+    {
+        "addressline1": "88 Northsouth Drive",
+        "addressline2": "Building C",
+        "city": "Dearborn",
+        "creditLimit": 100000,
+        "customerId": 777,
+        "discountCode": {
+            "discountCode": "L",
+            "rate": 7
+        },
+        "email": "www.westv.example.com",
+        "fax": "313-555-0171",
+        "name": "West Valley Inc.",
+        "phone": "313-555-0172",
+        "state": "MI",
+        "zip": "48128"
+    },
+    {
+        "addressline1": "2267 NE Michigan Ave",
+        "addressline2": "Building 21",
+        "city": "Dearborn",
+        "creditLimit": 5000000,
+        "customerId": 753,
+        "discountCode": {
+            "discountCode": "H",
+            "rate": 16
+        },
+        "email": "www.parts@ford.example.com",
+        "fax": "313-555-0152",
+        "name": "Zed Motor Co",
+        "phone": "313-555-0151",
+        "state": "MI",
+        "zip": "48128"
+    },
+    {
+        "addressline1": "52963 Notouter Dr",
+        "addressline2": "Suite 35",
+        "city": "Detroit",
+        "creditLimit": 50000,
+        "customerId": 722,
+        "discountCode": {
+            "discountCode": "N",
+            "rate": 0
+        },
+        "email": "www.bparts.example.com",
+        "fax": "313-555-0145",
+        "name": "Big Car Parts",
+        "phone": "313-555-0144",
+        "state": "MI",
+        "zip": "48124"
+    },
+    {
+        "addressline1": "4400 527th Street",
+        "addressline2": "Suite 562",
+        "city": "New York",
+        "creditLimit": 10000,
+        "customerId": 409,
+        "discountCode": {
+            "discountCode": "L",
+            "rate": 7
+        },
+        "email": "www.oldmedia.example.com",
+        "fax": "212-555-0111",
+        "name": "Old Media Productions",
+        "phone": "212-555-0110",
+        "state": "NY",
+        "zip": "10095"
+    },
+    {
+        "addressline1": "9653 211th Ave",
+        "addressline2": "Floor 4",
+        "city": "New York",
+        "creditLimit": 25000,
+        "customerId": 410,
+        "discountCode": {
+            "discountCode": "M",
+            "rate": 11
+        },
+        "email": "www.nycompltd@repair.example.com",
+        "fax": "212-555-0197",
+        "name": "Yankee Computer Repair Ltd",
+        "phone": "212-555-0191",
+        "state": "NY",
+        "zip": "10096"
+    }
+];
